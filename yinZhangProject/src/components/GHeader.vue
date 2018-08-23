d5a2e3a413d34129757b198b0d37bfd9c78fe8d6
zhujindu <1069899428@qq.com>
2018-08-23 09:52:14 +0800

    123

---
+++ yinZhangProject/src/components/GHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="GHeader">
+        <ul>
+            <li>系统管理员</li>
+            <li>2018-08-15</li>
+            <li>08：23：32</li>
+            <li><img src="../assets/u48.png" class="bg1"></img></li>
+            <li><img src="../assets/u42.png" class="bg2"></img></li>
+        </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #GHeader{
+        width:1311px;
+        height: 82px;
+        line-height: 82px;
+        position:absolute;
+        top: 0;
+        left: 175px;
+    }
+    ul{
+        float: right;
+    }
+    li{
+        float: left;
+        margin-right: 25px;
+    }
+    li> span{
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        border: 1px solid;
+        vertical-align: -3px;
+    }
+    li img{
+    vertical-align: middle;
+    }
+    li .bg1{
+        width: 27px;
+        height: 27px;
+    }
+    li .bg2{
+        width: 23px;
+        height: 23px;
+    }
+</style>
