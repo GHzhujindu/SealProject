af3bbba76271040fa56c04a259d7f2ab8188b745
ganxianjun <2431808449@qq.com>
2018-08-23 10:01:40 +0800

    444

---
+++ G/src/components/GSystemSetup.vue
@@ -0,0 +1,135 @@
+<template>
+<div>
+<div class="hi">
+	<el-row>
+	  
+	  <el-button type="info" plain>新建审批类型</el-button>
+	  
+</el-row>
+</div>
+  <el-table
+    :data="tableData2"
+    style="width: 100%"
+    :row-class-name="tableRowClassName">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="HHH">
+    </el-table-column>
+  </el-table>
+            <g-paper/>
+
+ </div>
+</template>
+
+<style scoped>
+.hi{
+	margin: 10px;
+	width: 100%;
+	height: 30px;
+	text-align:right;
+	padding-right: 20px;
+	margin-bottom: 20px;
+}
+.hi  button{
+	width: 155px;
+	height: 40px;
+
+}
+.hi  button span{
+	color: #000;
+}
+
+  .el-table .warning-row {
+  height:53px;
+  }
+.has-gutter th{
+	color: #000;
+	font-weight: bold;
+}
+ 
+  .GContent >div{
+  padding: 0 20px;
+  }
+  table tr{
+  height: 53px;
+  }
+  table tr:nth-of-type(odd){
+    background: #fff;
+
+  }
+  table tr:nth-of-type(even){
+    background: #fafafa;
+  	
+  }
+</style>
+
+<script>
+	import GPaper from './GPaper'
+  export default {
+
+      components: { 
+        GPaper
+       },
+    methods: {
+      tableRowClassName({row, rowIndex}) {
+        if (rowIndex === 1) {
+          return 'warning-row';
+        } else if (rowIndex === 3) {
+          return 'success-row';
+        }
+        return '';
+      },
+    },
+    data() {
+      return {
+        tableData2: [{
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        }]
+      }
+    }
+  }
+</script>
---
+++ G/src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="systermadministorOne">
+        <g-header />
+        <g-tabbar-left />
+        <div class="GContent"> <router-view /></div>
+    </div>
+</template>
+
+<script>
+import GHeader from './GHeader'
+import GTabbarLeft from './GTabbarLeft'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to ganXianJuns'
+    }
+  },
+  components: { 
+    GHeader,
+    GTabbarLeft
+   }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+h1, h2 {
+  font-weight: normal;
+}
+
+li {
+    list-style:none;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+    .systermadministorOne{
+        width: 100%;
+        height: 682px;
+        margin: 0 auto;
+    }
+    .systermadministorOne> div{
+        float: left;
+    }
+    .systermadministorOne_left{
+        width: 150px;
+        height: 100%;
+    }
+    .systermadministorOne_right{
+        width: 85%;
+        height: 100%;
+    }
+    .GContent{
+       position: absolute;
+        top: 82px;
+        left: 175px;
+        width:1311px;
+        height: 600px;
+        background: #e9f0f5;
+    }
+    table{
+        border-collapse: collapse;
+    }
+</style>
